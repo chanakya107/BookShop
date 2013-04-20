df68cbccc98ba9fd9363ff46d7107ac11d9cf704
Neha Rastogi <neharas@thoughtworks.com>
2013-04-20 12:12:09 +0530

    used database object to write into database in orderService | chethan - neha | f102

--- pustak/src/model/DataBase.java
+++ pustak/src/model/DataBase.java
@@ -24,7 +24,6 @@ public class DataBase {
         try {
             return statement.executeQuery(selectQuery);
         } catch (SQLException e) {
-            e.printStackTrace();
             return null;
         }
     }
@@ -73,4 +72,31 @@ public class DataBase {
     }
 
 
+    public void dropTable(String query) {
+        try {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DataBase dataBase = (DataBase) o;
+
+        if (connection != null ? !connection.equals(dataBase.connection) : dataBase.connection != null) return false;
+        if (statement != null ? !statement.equals(dataBase.statement) : dataBase.statement != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = connection != null ? connection.hashCode() : 0;
+        result = 31 * result + (statement != null ? statement.hashCode() : 0);
+        return result;
+    }
 }
--- pustak/src/services/OrderService.java
+++ pustak/src/services/OrderService.java
@@ -1,5 +1,7 @@
 package services;
 
+import model.DataBase;
+
 public interface OrderService {
-    void storeOrder(String customerName, String email, String phoneNumber, String address);
+    void storeOrder(String customerName, String email, String phoneNumber, String address, DataBase dataBase);
 }
