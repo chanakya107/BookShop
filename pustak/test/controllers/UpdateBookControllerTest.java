e8eb32bf0df4ee49e2e9b4b57f3f1c9cf90d7ed9
Pranav V Kudva <pranavkv@thoughtworks.com>
2013-04-24 18:14:16 +0530

    B106|wrote test for UpdateBookController|Pranav,Melanie

--- pustak/test/controllers/UpdateBookControllerTest.java
+++ pustak/test/controllers/UpdateBookControllerTest.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import model.Book;
 import org.junit.Assert;
 import org.junit.Test;
 import services.BookService;
+import services.impl.BookServiceImpl;
 import step.web.framework.WebContext;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class UpdateBookControllerTest {
 
@@ -22,6 +26,22 @@ public class UpdateBookControllerTest {
         BookService bookService=null;
         WebContext context=mock(WebContext.class);
         UpdateBookController.createUpdateBookController(context,bookService);
+    }
+
+    @Test
+    public void update_calls_bind(){
+        WebContext context = mock(WebContext.class);
+        BookServiceImpl bookService = mock(BookServiceImpl.class);
+        String keyisbn = "123";
+        String status="New";
+        UpdateBookController controller = UpdateBookController.createUpdateBookController(context, bookService);
+        when(context.requestBodyField("isbn")).thenReturn(keyisbn);
+        when(context.requestBodyField("bookstatus")).thenReturn(status);
+        String additionalCopies="1";
+        when(context.requestBodyField("AdditionalCopies")).thenReturn(additionalCopies);
+        controller.update();
+        verify(bookService).updateStock(Integer.parseInt(additionalCopies),keyisbn,status);
+        verify(context).bind("isbn",keyisbn);
     }
 
 }
