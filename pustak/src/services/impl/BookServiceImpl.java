2506f5fa723919a1a6b1fdaf34538cd8fba8fc6f
Pranav V Kudva <pranavkv@thoughtworks.com>
2013-04-24 20:27:57 +0530

    Fixed bug:Always Out of stock for used books|Melanie,Pranav,Neha

--- pustak/src/services/impl/BookServiceImpl.java
+++ pustak/src/services/impl/BookServiceImpl.java
@@ -17,20 +17,6 @@ public class BookServiceImpl implements BookService {
         this.dataBase = dataBase;
     }
 
-    @Override
-    public Book[] displayAllBooks(String type, String searchKey) {
-        dataBase.connectTo("pustak.db");
-        if (searchKey.equals(""))
-            return buildDisplayBooks(dataBase.selectQuery("select isbn,title,author1,author2," + getCategory(type) + " from books"));
-        return buildResultBooks(dataBase.selectQuery("select isbn,title,author1,author2," + getCategory(type) + " from books"), searchKey);
-    }
-
-    private String getCategory(String type) {
-        if (type.equals("New"))
-            return "price,newbookquantity";
-        return "price/2,usedbookquantity";
-    }
-
     public boolean addBook(Book book) {
         dataBase.connectTo("pustak.db");
         dataBase.createBooksTable();
@@ -75,42 +61,74 @@ public class BookServiceImpl implements BookService {
         return foundBook;
     }
 
-    private Book[] buildResultBooks(ResultSet rs, String searchKey) {
+    private Book[] buildResultBooks(ResultSet rs, String searchKey, String type) {
         List<Book> books = new ArrayList<Book>();
         try {
             while (rs.next()) {
                 if (rs.getString(2).toLowerCase().contains(searchKey.toLowerCase())) {
-                    books.add(getBook(rs));
+                    getBooks(rs, type, books);
+
                 }
             }
-            return books.toArray(new Book[books.size()]);
         } catch (SQLException e) {
-            return null;
+            e.printStackTrace();
         } finally {
             dataBase.closeConnection();
         }
+        return books.toArray(new Book[books.size()]);
     }
 
-    private Book[] buildDisplayBooks(ResultSet rs) {
+     @Override
+    public Book[] displayAllBooks(String type, String searchKey) {
+        dataBase.connectTo("pustak.db");
+        if (searchKey.equals(""))
+            return buildDisplayBooks(dataBase.selectQuery("select isbn,title,author1,author2," + getCategory(type) + " from books"), type);
+         else
+        return buildResultBooks(dataBase.selectQuery("select isbn,title,author1,author2," + getCategory(type) + " from books"), searchKey, type);
+    }
+
+    private String getCategory(String type) {
+        if (type.equals("New"))
+            return "price,newbookquantity";
+        return "price/2,usedbookquantity";
+    }
+
+    private Book[] buildDisplayBooks(ResultSet rs, String type) {
         List<Book> books = new ArrayList<Book>();
         try {
             while (rs.next()) {
-                books.add(getBook(rs));
+                getBooks(rs, type, books);
             }
-            return books.toArray(new Book[books.size()]);
         } catch (SQLException e) {
-            return null;
+            e.printStackTrace();
         } finally {
             dataBase.closeConnection();
         }
+        return books.toArray(new Book[books.size()]);
     }
 
-    private Book getBook(ResultSet rs) throws SQLException {
+    private void getBooks(ResultSet rs, String type, List<Book> books) throws SQLException {
+        if(type.equals("New"))
+           books.add(getNewBooks(rs));
+        else
+         books.add(getUsedBooks(rs));
+    }
+
+    private Book getNewBooks(ResultSet rs) throws SQLException {
         return new Book(rs.getString(1),
                 rs.getString(2).replace("+", " "),
                 rs.getString(3).replace("+", " "),
                 rs.getString(4).replace("+", " "),
                 rs.getInt(5),
                 rs.getInt(6), 0);
+    }
+
+    private Book getUsedBooks(ResultSet rs) throws SQLException{
+        return new Book(rs.getString(1),
+                rs.getString(2).replace("+", " "),
+                rs.getString(3).replace("+", " "),
+                rs.getString(4).replace("+", " "),
+                rs.getInt(5),
+                0, rs.getInt(6));
     }
 }
