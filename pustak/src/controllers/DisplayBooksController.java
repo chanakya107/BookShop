75e0737f58922ed4166c203423f4c1fd16eb5765
Chethan A <chethana@thoughtworks.com>
2013-04-24 19:12:03 +0530

    added no books found message when search result is 0 | chethan - mani | b38

--- pustak/src/controllers/DisplayBooksController.java
+++ pustak/src/controllers/DisplayBooksController.java
@@ -1,17 +1,11 @@
 package controllers;
 
+import model.Book;
 import services.BookService;
 import step.web.framework.RequestHandlerResult;
 import step.web.framework.WebContext;
 import views.ViewTemplates;
 
-/**
- * Created with IntelliJ IDEA.
- * User: neharas
- * Date: 23/4/13
- * Time: 4:02 PM
- * To change this template use File | Settings | File Templates.
- */
 public class DisplayBooksController {
     private final WebContext context;
     private final BookService bookService;
@@ -22,10 +16,20 @@ public class DisplayBooksController {
     }
 
     public RequestHandlerResult list() {
-
         String searchKey=context.requestBodyField("searchKey");
-        context.bind("new_books", bookService.displayAllBooks("New",searchKey));
-        context.bind("used_books", bookService.displayAllBooks("Used", searchKey));
+        Book[] newBooks = bookService.displayAllBooks("New", searchKey);
+        Book[] usedBooks = bookService.displayAllBooks("Used", searchKey);
+        context.bind("status",getStatus(newBooks));
+        context.bind("new_books", newBooks);
+        context.bind("used_books", usedBooks);
         return RequestHandlerResult.ok(context.render(ViewTemplates.Index));
+    }
+
+    private String getStatus(Book[] newBooks) {
+        String status = "";
+        if (newBooks.length == 0){
+            status = "No Books Found!!!";
+        }
+        return status;
     }
 }
