0b12246ccd9363eadfbcf01aa72593efc08384c6
Pranav V Kudva <pranavkv@thoughtworks.com>
2013-04-24 18:26:43 +0530

    Fixed bug in restriction of price and quantity before a book is added to 10 digits|Pranav,Melanie

--- pustak/src/controllers/DisplayBooksController.java
+++ pustak/src/controllers/DisplayBooksController.java
@@ -5,13 +5,6 @@ import step.web.framework.RequestHandlerResult;
 import step.web.framework.WebContext;
 import views.ViewTemplates;
 
-/**
- * Created with IntelliJ IDEA.
- * User: neharas
- * Date: 23/4/13
- * Time: 4:02 PM
- * To change this template use File | Settings | File Templates.
- */
 public class DisplayBooksController {
     private final WebContext context;
     private final BookService bookService;
